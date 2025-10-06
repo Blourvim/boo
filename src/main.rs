f64e90f129c703f23a34a5e52291c093e0b8fa52
blourvim <blourvim@gmail.com>
2025-10-07 01:25:16 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,24 @@
 
-
-
-
-
-
-
-
-                                        const
-                                        PAGE:
-                                 &str =include_str!("index.html");
-                                       const PAYLOAD: &str = include_str!("index.html");
-                                      use std::io::{BufRead, Write};use std::{thread, time};
-
-fn main() {
-    let listener = std::net::TcpListener::bind("0.0.0.0:3000").unwrap();
-    for mut stream in listener.incoming().flatten() {
-        let mut rdr = std::io::BufReader::new(&mut stream);
-        let mut l = String::new();
-        rdr.read_line(&mut l).unwrap();
-
-        match l.trim().split(' ').collect::<Vec<_>>().as_slice() {
-            ["GET", _, "HTTP/1.1"] => {
-                send_html(&mut stream)
-            }
-            ["POST", resource, "HTTP/1.1"] => match resource {
-_=>todo!()
-
-            },
-
-            _ => {}
-        }
-    }
-}
-
-fn send_html(stream: &mut std::net::TcpStream,) {
-    stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
-    stream.write_all(b"\n").unwrap();
-
-
-for page_byte in PAGE.as_bytes(){
-
-  // stream.write_all(page_byte);
-
-}
-
-    stream.shutdown(std::net::Shutdown::Both).unwrap()
-}
-
+                                                use std::io::{BufRead
+                                        , Write};fn main(){let listener = 
+                                    std::net::TcpListener::bind("0.0.0.0:3001")
+                                  .unwrap();for mut stream in listener.incoming().
+                                  flatten()         {let mut rdr        = std::io::
+                                  BufReader         ::new(&mut         stream);let
+                                  mut list           = String ::        new();rdr.
+                                  read_line         (&mut list)        .unwrap();
+                                  match list.trim().split(' ').collect::<Vec<_>>()
+                                     .as_slice(){["GET",      _, "HTTP/1.1"] =>
+                                   { stream.write_all            (b"HTTP/1.1 200
+                                   OK\r\n").unwrap();           stream.write_all
+                                    (b"Content-Type: text/plain; charset=utf-8
+                                    \r\n\r\n").unwrap();stream.write_all(b"\n").
+                                      unwrap (); stream.write_all(b"\n").unwrap();
+                                            stream.write_all(b"BOO!!!")
+                                      .unwrap()  ;let source =    std::fs::
+                                read_to_string    (file!()).          unwrap();
+                                                 stream.
+                                write_all(    source
+                                   .as_bytes()).
+                                      unwrap();
+                                   ;} _ => {}}}}
