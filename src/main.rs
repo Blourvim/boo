0226d51c1aa7ad6dd16095c21a173e49855f307d
blourvim <blourvim@gmail.com>
2025-10-07 21:07:43 +0300

    add some css

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,23 @@
                                   BufReader         ::new(&mut           stream);let  
                                   mut list           = String ::          new();rdr.
                                   read_line         (&mut list)         .unwrap();   
-                                  match list.trim().split(' ').collect::<Vec<_>>()
-                                     .as_slice(){["GET",      _, "HTTP/1.1"] =>
-                                  {let _=0;stream.write_all(b"HTTP/1.1 200 OK\r\n").
-                                   unwrap() ; let _booo = 8001 ;stream.write_all
-                                    (  b"Content-Type: text/html; charset=utf-8
-                                    \r\n\r\n").unwrap();stream.write_all(b"\n").
-                                     unwrap();stream.write_all(b"\n").unwrap();
-                                       const S: &str = include_str!("main.rs");
-                                       stream.write_all(S.as_bytes()).unwrap();
-                                                                /*
-<style>
-html, body {
-    margin: 0;
-    background-color:black; 
-    color: #f8f1e5;
-    font-family: "Courier New", monospace;
-    animation-name:myAnimation;
-    animation-duration:4s;
-}
-@keyframes myAnimation {
-  from {color: #f8f1e5;}
-  to {color: black;}
-}
-
-
-</style> */
-            } _ => {}}}}
+                                  let lsl = list.trim(); let _= 0;match lsl.split
+                (' ').            collect::<Vec<_>>().as_slice(){["GET",  _,"HTTP/1.1"]       =>{stream
+              .write_all          (b"HTTP/1.1 200 OK           \r\n").unwrap();        let _8001=1;
+                   stream         .write_all(b"Content-Type: text/html; charset=utf-8\r\n\r\n")
+                .unwrap();stream.write_all(b"\n").unwrap();stream.write_all(b"\n").unwrap();
+                      const S: &str = include_str!("main.rs");stream.write_all(S.as_bytes())
+                                     .unwrap();let x = r#"/*<style>html, body {margin: 0;
+                                       background-color:black;color: #f8f1e5;
+                                       font-family: "Courier New", monospace;
+                                          animation-name:myAnimation;
+                                           animation-duration:4s;}
+                                          @keyframes myAnimation {
+                                        from {color: #f8f1e5;}
+                                        to {color: black;}}
+                                          </style> */"#;
+                                stream.write_all("<pre>".as_bytes()).unwrap();
+                                stream.write_all("<ayayayaya".as_bytes()).unwrap();
+                                stream.write_all(x.as_bytes()).unwrap();
+                                stream.write_all("</pre>".as_bytes()).unwrap();
+                                        } _ => {}}}}
